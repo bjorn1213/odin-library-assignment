8ca1d73d15e546dc4284d6abad0182443f4780ed
Bjorn <11698329+bjorn1213@users.noreply.github.com>
2023-03-26 12:38:31 +0200

    Implement add dummy book

--- script.js
+++ script.js
@@ -15,12 +15,26 @@ for (let i = 0; i < 10; i++) {
   addBookToLibrary(new Book("dummyTitle", "dummyAuthor", 200, true));
 }
 
-const table = document.querySelector("#book-table");
+function generateLibrary() {
+  const oldTbody = document.querySelector("#book-table tbody");
+  const newTbody = document.createElement("tbody");
 
-myLibrary.forEach((book) => {
-  const row = table.insertRow();
-  row.insertCell().innerHTML = book.title;
-  row.insertCell().innerHTML = book.author;
-  row.insertCell().innerHTML = book.pageCount;
-  row.insertCell().innerHTML = book.read ? "Yes" : "No";
+  myLibrary.forEach((book) => {
+    const row = newTbody.insertRow();
+    row.insertCell().innerHTML = book.title;
+    row.insertCell().innerHTML = book.author;
+    row.insertCell().innerHTML = book.pageCount;
+    row.insertCell().innerHTML = book.read ? "Yes" : "No";
+  });
+
+  oldTbody.parentNode.replaceChild(newTbody, oldTbody);
+}
+
+generateLibrary();
+
+const addButton = document.querySelector("button");
+
+addButton.addEventListener("click", () => {
+  addBookToLibrary(new Book("dummyTitle", "dummyAuthor", 200, true));
+  generateLibrary();
 });
