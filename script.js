eebf388d0b25407f8adcade754c92891bc3f2611
Bjorn <11698329+bjorn1213@users.noreply.github.com>
2023-03-26 13:11:55 +0200

    Implement basic book adding functionality

--- script.js
+++ script.js
@@ -11,10 +11,6 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-for (let i = 0; i < 10; i++) {
-  addBookToLibrary(new Book("dummyTitle", "dummyAuthor", 200, true));
-}
-
 function generateLibrary() {
   const oldTbody = document.querySelector("#book-table tbody");
   const newTbody = document.createElement("tbody");
@@ -30,11 +26,39 @@ function generateLibrary() {
   oldTbody.parentNode.replaceChild(newTbody, oldTbody);
 }
 
+function toggleAddBookPrompt() {
+  const overlayElement = document.querySelector(".overlay");
+
+  if (overlayElement.style.display === "none") {
+    overlayElement.style.display = "flex";
+  } else {
+    overlayElement.style.display = "none";
+  }
+}
+
+for (let i = 0; i < 1; i++) {
+  addBookToLibrary(new Book("dummyTitle", "dummyAuthor", 200, true));
+}
 generateLibrary();
 
-const addButton = document.querySelector("button");
+const addButton = document.querySelector(".add-book");
 
 addButton.addEventListener("click", () => {
-  addBookToLibrary(new Book("dummyTitle", "dummyAuthor", 200, true));
+  toggleAddBookPrompt();
+});
+
+const confirmAddBook = document.querySelector("#add-book");
+confirmAddBook.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const bookTitle = document.getElementById("bookTitle").value;
+  const bookAuthor = document.getElementById("bookAuthor").value;
+  const pageCount = document.getElementById("pageCount").value;
+  const read = document.getElementById("read").value;
+
+  addBookToLibrary(new Book(bookTitle, bookAuthor, pageCount, read));
   generateLibrary();
+
+  toggleAddBookPrompt();
+  document.getElementById("form-add-book").reset();
 });
